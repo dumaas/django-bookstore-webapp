8033791f39c7d4b5981b98f49ed4041e2760c189
dumaas <normify@gmail.com>
2021-01-12 10:53:12 -0600

    secure_proxy_ssl_header and allowed_hosts update

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='False')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-bookstore-webapp.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -206,6 +206,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # Just in case...
     DEBUG = False
 
